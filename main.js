4dbd5dbe719556b66bb8b8c23f6aaf8eb27eaa38
Beardless_sheik <44676055+Beardless-sheik@users.noreply.github.com>
2021-09-18 19:04:30 +0200

    Add basic UI for new nav links and changing content dynamically

--- main.js
+++ main.js
@@ -7,8 +7,17 @@ class BookList {
     }
     this.bookListContainer = document.querySelector('#book-list');
     this.addButtonElement = document.querySelector('#add-button');
+    //  Nav Elements Selected
+    this.listElementNavSelection = document.querySelector('#list-nav-text');
+    this.addBookNavSelection = document.querySelector('#add-new-nav-text');
+    this.contactElementNavSelection = document.querySelector('#contact-nav-text');
+    //  Section Elements Selected to changes displays as per nav links
+    this.listSection = document.querySelector('#list-section');
+    this.addNewSection = document.querySelector('#add-new-section');
+    this.contactSection = document.querySelector('#contact-section');
     this.addEventlisteners();
     this.listBooks();
+    this.initDisplayFunction();
   }
 
   addBooks(book) {
@@ -59,8 +68,36 @@ class BookList {
       }
       this.listBooks();
     };
+
+    const listBookNavEventFunction = () => {
+      this.listSection.style.display = 'block';
+      this.addNewSection.style.display = 'none';
+      this.contactSection.style.display = 'none';
+    };
+
+    const addNewBookNavEventFunction = () => {
+      this.listSection.style.display = 'none';
+      this.addNewSection.style.display = 'block';
+      this.contactSection.style.display = 'none';
+    };
+
+    const contactSectionNavEventFunction = () => {
+      this.listSection.style.display = 'none';
+      this.addNewSection.style.display = 'none';
+      this.contactSection.style.display = 'block';
+    };
+
     this.addButtonElement.addEventListener('click', addBookEventFunction);
     this.bookListContainer.addEventListener('click', removeBookEventFunction);
+    this.listElementNavSelection.addEventListener('click', listBookNavEventFunction);
+    this.addBookNavSelection.addEventListener('click', addNewBookNavEventFunction);
+    this.contactElementNavSelection.addEventListener('click', contactSectionNavEventFunction);
+  }
+
+  initDisplayFunction = () => {
+    this.listSection.style.display = 'block';
+    this.addNewSection.style.display = 'none';
+    this.contactSection.style.display = 'none';
   }
 }
 // eslint-disable-next-line no-unused-vars
